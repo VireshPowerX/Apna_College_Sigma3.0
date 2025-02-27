0f682a54560d2ac27bc5f3dcf438f6f5a4fb559b
VireshPowerX <107186832+VireshPowerX@users.noreply.github.com>
2025-02-27 12:21:10 +0530

    .

--- Sigma_Batch_Developer/Javascript/Javascript1.js
+++ Sigma_Batch_Developer/Javascript/Javascript1.js
@@ -1,1 +1,6 @@
 /* Comments in javascript and css are same but in html <!-- Comments in html --> */
+console.log("Hello World");
+console.log(variable);
+console.log(10+20);
+console.log("Hello","World",variable,(10-20),"!");
+console.log(`Hello World ${variable} ${10+20} !`);
