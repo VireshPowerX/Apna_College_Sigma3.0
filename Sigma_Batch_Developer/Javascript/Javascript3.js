2d59573c38c8e61c9d9779e7d6850c6bd42fd299
VireshPowerX <107186832+VireshPowerX@users.noreply.github.com>
2025-03-09 00:12:05 +0530

    .

--- Sigma_Batch_Developer/Javascript/Javascript3.js
+++ Sigma_Batch_Developer/Javascript/Javascript3.js
@@ -1,91 +1,118 @@
-// for(intialization; condition; increment/decrement){} // syntax of for loop
-for(let i=1; i<=5; i++){ // for loop to print 1 to 5
-    console.log(i); // 1 2 3 4 5
+// For Loop Syntax: for(initialization; condition; increment/decrement){}
+// For loop to print numbers from 1 to 5
+for(let i = 1; i <= 5; i++) { 
+    console.log(i); // Output: 1 2 3 4 5
 }
-/*for(let i=1; i=i+5;){ // for loop to print infinite numbers
-    console.log(i); // infinite numbers
+// -----------------------------------------------------------------------------------------------
+/*
+// Infinite loops due to incorrect conditions
+for(let i = 1; i = i + 5;) { // i is always reassigned, causing an infinite loop
+    console.log(i); 
 }
-for(let i=1; i<=5; i--){ // for loop to print 1 to 5
-    console.log(i); // infinite numbers
+
+for(let i = 1; i <= 5; i--) { // i is decreasing, never reaching 5
+    console.log(i); 
 }
-for(let i=1; i>0; i++){ // for loop to print infinite numbers
-    console.log(i); // infinite numbers
+
+for(let i = 1; i > 0; i++) { // Condition is always true, infinite loop
+    console.log(i); 
 }
 */
-for(let i=1; i++ <=5;){ // for loop to print 1 to 5
-    console.log(i); // 2 3 4 5 6
+// -----------------------------------------------------------------------------------------------
+// Incorrect increment placement leading to unexpected output
+for(let i = 1; i++ <= 5;) { 
+    console.log(i); // Output: 2 3 4 5 6
 }
-let n = prompt("Enter a number: "); // prompt to enter a number
-n = parseInt(n); // converting string to integer
-for(let i=n; i<=n*10; i=i+n){ // for loop to print table of entered number
-    console.log(i); // multiplication table of entered number
+// -----------------------------------------------------------------------------------------------
+// Multiplication table of a user-entered number
+let n = prompt("Enter a number: "); // Prompt user for input
+n = parseInt(n); // Convert input string to an integer
+for(let i = n; i <= n * 10; i = i + n) { // Loop to print the multiplication table
+    console.log(i); // Output: n, 2n, 3n, ..., 10n
 }
-//nested for loop
-for(let i=1; i<=3; i++){
-    console.log(`outer ${i}`);
-    for(let j=1; j<=3; j++){
-        console.log(j);
+// -----------------------------------------------------------------------------------------------
+// Nested for loop example
+for(let i = 1; i <= 3; i++) {
+    console.log(`Outer loop iteration ${i}`); // Outer loop message
+    for(let j = 1; j <= 3; j++) {
+        console.log(j); // Inner loop output: 1 2 3
     }
 }
-//while(condition){} // syntax of while loop
-let i = 1; // initializing i to 1
-while(i<=5){ // while loop to print 1 to 5
-    console.log(i); // 1 2 3 4 5
-    i++; // incrementing i
+// -----------------------------------------------------------------------------------------------
+// While Loop Syntax: while(condition){}
+// Using while loop to print numbers from 1 to 5
+let i = 1; // Initialize i to 1
+while(i <= 5) { 
+    console.log(i); // Output: 1 2 3 4 5
+    i++; // Increment i
 }
-while(i<=5){
-    if(i==3){
-        break; // break statement to exit the loop
+// -----------------------------------------------------------------------------------------------
+// Using break in a while loop
+while(i <= 5) {
+    if(i == 3) {
+        break; // Exits the loop when i equals 3
     }
-    console.log(i); // 1 2
+    console.log(i); // Output: 1 2
 }
-//array with for loop
-let fruits = ['apple', 'banana', 'mango', 'orange']; // array of fruits
-for(let i=0; i<fruits.length; i++){ // for loop to print array elements
-    console.log(i,fruits[i]); // apple banana mango orange
+// -----------------------------------------------------------------------------------------------
+// Iterating over an array using a for loop
+let fruits = ['apple', 'banana', 'mango', 'orange']; // Array of fruits
+for(let i = 0; i < fruits.length; i++) { 
+    console.log(i, fruits[i]); // Output: index and fruit name
 }
-//nested for loop with nested array
-let heroes = [["superman", "batman", "wonderwoman"], ["ironman", "captainamerica", "thor"], ["spiderman", "hulk", "blackwidow"]]; // nested array of heroes
-for(let i=0; i<heroes.length; i++){ // outer for loop
-    console.log(`outer ${i}`);
-    for(let j=0; j<heroes[i].length; j++){ // inner for loop
-        console.log(heroes[i][j]); // superman batman wonderwoman ironman captainamerica thor spiderman hulk blackwidow
-    }
-}
-//for (variable of iterable){} // syntax of for of loop
-let colors = ['red', 'green', 'blue', 'yellow']; // array of colors
-for(let color of colors){ // for of loop to print array elements
-    console.log(color); // red green blue yellow
-}
-let name1 = "javascript"; // string
-for(let char of name1){ // for of loop to print string characters
-    console.log(char); // j a v a s c r i p t
-}
-//nested for of loop
-let nestedColors = [['red', 'green'], ['blue', 'yellow'], ['pink', 'orange']]; // nested array of colors
-for(let colors of nestedColors){ // outer for of loop
-    console.log(`outer ${colors}`);
-    for(let color of colors){ // inner for of loop
-        console.log(color); // red green blue yellow pink orange
+// -----------------------------------------------------------------------------------------------
+// Nested for loop with a nested array
+let heroes = [
+    ["superman", "batman", "wonderwoman"],
+    ["ironman", "captainamerica", "thor"],
+    ["spiderman", "hulk", "blackwidow"]
+];
+for(let i = 0; i < heroes.length; i++) { // Outer loop
+    console.log(`Outer array index ${i}`);
+    for(let j = 0; j < heroes[i].length; j++) { // Inner loop
+        console.log(heroes[i][j]); // Output: superhero names
     }
 }
-//todo app
-let todos = []; // empty array
-let input = prompt("Enter a todo: "); // prompt to enter a todo
-while(input !== 'quit'){ // while loop to add todos
-    if(input === 'list'){ // if input is list
+// -----------------------------------------------------------------------------------------------
+// For...of loop syntax: for (variable of iterable){}
+// Iterating over an array using for...of loop
+let colors = ['red', 'green', 'blue', 'yellow'];
+for(let color of colors) { 
+    console.log(color); // Output: red green blue yellow
+}
+// -----------------------------------------------------------------------------------------------
+// Iterating over a string using for...of loop
+let name1 = "javascript";
+for(let char of name1) {
+    console.log(char); // Output: j a v a s c r i p t
+}
+// -----------------------------------------------------------------------------------------------
+// Nested for...of loop with a nested array
+let nestedColors = [['red', 'green'], ['blue', 'yellow'], ['pink', 'orange']];
+for(let colors of nestedColors) { // Outer loop
+    console.log(`Outer array: ${colors}`);
+    for(let color of colors) { // Inner loop
+        console.log(color); // Output: red green blue yellow pink orange
+    }
+}
+// -----------------------------------------------------------------------------------------------
+// Simple Todo App using loops
+let todos = []; // Empty array to store todos
+let input = prompt("Enter a todo: "); // Prompt user for input
+while(input !== 'quit') { // Loop continues until user types 'quit'
+    if(input === 'list') { // If user types 'list', display all todos
         console.log("**********");
-        for(let i=0; i<todos.length; i++){ // for loop to print todos
-            console.log(`${i}: ${todos[i]}`);
+        for(let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`); // Display todos with index
         }
         console.log("**********");
-    }else if(input === 'delete'){ // if input is delete
-        let index = prompt("Enter index to delete: "); // prompt to enter index to delete
-        todos.splice(index, 1); // deleting todo
+    } else if(input === 'delete') { // If user types 'delete'
+        let index = prompt("Enter index to delete: "); // Prompt for index
+        todos.splice(index, 1); // Remove the todo at given index
         console.log("Todo deleted");
-    }else{
-        todos.push(input); // adding todo to array
+    } else {
+        todos.push(input); // Add new todo to the array
         console.log("Todo added");
     }
-    input = prompt("Enter a todo: "); // prompt to enter a todo
+    input = prompt("Enter a todo: "); // Ask for next input
 }
