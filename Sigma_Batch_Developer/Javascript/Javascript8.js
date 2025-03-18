ea79d330bc1a1489f21fba7a2bb9eab6199b592c
VireshPowerX <107186832+VireshPowerX@users.noreply.github.com>
2025-03-18 13:33:30 +0530

    .

---
+++ Sigma_Batch_Developer/Javascript/Javascript8.js
@@ -0,0 +1,14 @@
+// variable and data-types if-else statements loops object literals functions now we merge this js with html and css
+// Dom Document object model logical tree
+// every html element convert into object js
+console.dir(document.all);
+document.getElementById("input");
+document.getElementsByClassName("image"); // a HTML Collection we got its similar to array but we can't use array methods
+document.getElementsByTagName("p");
+document.querySelector('div p'); // for first div paragraph return
+document.querySelector('#myId');
+document.querySelector('.myClass');
+document.querySelectorAll("p");
+p.innerText; // Shows the visible text contained in a node
+p.innerHTML; // Shows all the full text (display:hidden seen)
+p.textContent; // Shows the full markup (bold italic seen)
