03d9687a6271c05af1ebfe5fc948d9bb920a92e7
Viresh Dashrath Panchal <vireshpanchal18@gmail.com>
2025-03-25 20:47:44 +0530

    .

--- Sigma_Batch_Developer/Javascript/Javascript10.js
+++ Sigma_Batch_Developer/Javascript/Javascript10.js
@@ -10,18 +10,21 @@ document.getElementById("child").addEventListener("click", function (event) {
 });
 // Event Delegation: Instead of attaching event listeners to multiple elements, you attach a single event listener to a parent element and delegate the event handling to the child elements.
 document.getElementById("parent").addEventListener("click", function (event) {
-    if (event.target && event.target.matches("button")) {
-        console.log("Button clicked:", event.target.textContent);
-    }
+  if (event.target && event.target.matches("button")) {
+    console.log("Button clicked:", event.target.textContent);
+  }
 });
 // Advance js
 // Async functions Call Stack
 // LIFO Last In First Out
-function hello() {};
+function hello() {}
 // Call stack first demo work last hello work
-function demo() {hello()} demo();
+function demo() {
+  hello();
+}
+demo();
 // Breakpoints
-// inspect sources .js select number and reload 
+// inspect sources .js select number and reload
 // syncronize means line wise work document function
 // asyncronize means some work after document happens like settimeout
 // JS is Single Threaded language
@@ -32,18 +35,45 @@ function demo() {hello()} demo();
 // promises is object in resolve & reject
 function savetoDb(data) {
   return new Promise((resolve, reject) => {
-    let internetSpeed = Math.floor(Math.random()*10)+1;
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
     if (internetSpeed > 4) {
       resolve("success : data was saved");
-    } else  {
+    } else {
       reject("error : data couldn't be saved");
     }
   });
-};
+}
 // promises then() & catch()
 savetoDb("AI")
-.then(() => {
-  console.log("promise was resolved");
-}) .catch(() => {
-  console.log("promise was rejected");
-});
+  .then(() => {
+    console.log("promise was resolved");
+  })
+  .catch(() => {
+    console.log("promise was rejected");
+  });
+// Promises chaining
+savetoDb("AI")
+  .then(() => {
+    console.log("promise resolved");
+    return savetoDb("ML");
+  })
+  .then(() => {
+    console.log("second promise resolved");
+    return savetoDb("DL");
+  })
+  .catch(() => {
+    console.log("promise rejected");
+  });
+// Promise result & error
+savetoDb("AI")
+  .then((result) => {
+    console.log("promise resolved", result);
+    return savetoDb("ML");
+  })
+  .then((result) => {
+    console.log("second promise resolved", result);
+    throw new Error("Error: Deep Learning failed");
+  })
+  .catch((error) => {
+    console.log("promise rejected", error);
+  });
